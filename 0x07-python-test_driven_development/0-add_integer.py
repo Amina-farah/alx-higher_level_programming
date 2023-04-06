d16fea9ec5ae1ee1b28cf65cddd7fe5aebe4c51e
Amina-farah <aminaali5580@gmail.com>
2023-04-06 10:55:48 -0700

    update

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,14 @@
+#!usr/bin/python3
+""defines an integer addition function""
+
+def add_integer(a, b=98):
+    ""Return the integer addition of a and b.
+    float arguments are typecasted to ints before addition is performed
+    Raises:
+        TypeError:if either of a or b is a non-integer and non-float
+        ""
+        if ((not isinstance(a, int) and not isinstance(a, float))):
+            raise TypeError("a must be an integer")
+        if ((not isinstances(b, int) and not isinstance(b, float)):
+                Raise TypeError("b, must be an integer")
+                return(int(a)+int(b))
