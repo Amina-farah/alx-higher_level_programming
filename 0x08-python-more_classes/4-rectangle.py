505328bc61e341c55f1f5ec79b9904ef84bf7f52
Amina-farah <aminaali5580@gmail.com>
2023-04-06 00:49:23 -0700

    update

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """Defines a Rectangle class."""
 
-
 class Rectangle:
     """Represent a rectangle."""
 
     def __init__(self, width=0, height=0):
         """Initialize a new Rectangle.
+
         Args:
             width (int): The width of the new rectangle.
             height (int): The height of the new rectangle.
@@ -52,10 +52,12 @@ class Rectangle:
 
     def __str__(self):
         """Return the printable representation of the Rectangle.
+
         Represents the rectangle with the # character.
         """
         if self.__width == 0 or self.__height == 0:
             return ("")
+
         rect = []
         for i in range(self.__height):
             [rect.append('#') for j in range(self.__width)]
@@ -64,6 +66,7 @@ class Rectangle:
         return ("".join(rect))
 
     def __repr__(self):
-        """Return a string representation of the Rectangle."""
-        return "Rectangle({}, {})".format(self.__width, self.__height)
-
+        """Return the string representation of the Rectangle."""
+        rect = "Rectangle(" + str(self.__width)
+        rect += ", " + str(self.__height) + ")"
+        return (rect)
